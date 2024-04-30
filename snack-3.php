91eac2b3208bf6b3b9126326283171d568e3e125
danielbosa <danielbosa95@gmail.com>
2024-04-30 22:38:50 +0200

    Snack 2 fixed

--- snack-3.php
+++ snack-3.php
@@ -1,17 +1,5 @@
 <?php
-
 /*
-Snack 2
-Con un form passare come parametri GET name, mail e age.
-==> ho bisogno di un form con 3 input (text per name, mail per mail, number per age).
-e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero.
-=> controlli:
-- name len > 3; 
-- mail contains "." e "@";
-- age is number;
-Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
-=> print string
-
 Snack 3
     Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post.
     Qui l’array di esempio: https://www.codepile.net/pile/R2K5d68z
@@ -24,17 +12,6 @@ Snack 4
 // if(isset($name) && isset($email) && isset($age)){
 
 // };
-if(!empty($_GET)){
-    $name = $_GET['name'];
-    $email = $_GET['email'];
-    $age = $_GET['age'];
-    if(strlen($name) < 3 || !str_contains($email, '@') && !str_contains($email, '.') || !is_numeric($age) ){
-        echo "Accesso negato";
-    } else {
-        echo "Accesso riuscito!";
-    }
-};
-
 
 ?>
 
